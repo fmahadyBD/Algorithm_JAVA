0ff9eb313cb4c443467b48a4db3319a0d9d3d5e0
Mahady Hasan Fahim <fmahady01@gmail.com>
2024-05-31 18:44:50 +0600

    added DFS and Marge sort

---
+++ src/DFS.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class DFS {
+
+    private int V;
+    private LinkedList<Integer> adj[];
+    private ArrayList<Integer> levels;
+    private Stack<Integer> s;
+
+    DFS(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        levels = new ArrayList<>(v);
+        s = new Stack<>();
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+            levels.add(-1);
+        }
+
+    }
+
+    void dfscode(int start) {
+        s.push(start);
+        boolean[] vis = new boolean[V];
+        levels.set(start, 0);
+
+        while (!s.isEmpty()) {
+            int x = s.pop();
+            if (!vis[x]) {
+                vis[x] = true;
+                System.out.println(x);
+                for (int i : adj[x]) {
+                    if (!vis[i]) {
+                        s.push(i);
+                        if (levels.get(i) == -1) {
+                            levels.set(i, levels.get(x) + 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the total vertex number: ");
+        int v = sc.nextInt();
+        DFS graph = new DFS(v);
+
+        System.out.println("Enter the total edges: ");
+        int e = sc.nextInt();
+        for (int i = 0; i < e; i++) {
+            System.out.println("Enter the x ,y :");
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            graph.adj[x].add(y);
+            graph.adj[y].add(x);
+        }
+        graph.dfscode(0);
+    }
+
+}
---
+++ src/MargeSort.java
@@ -0,0 +1,55 @@
+public class MargeSort {
+
+    public static void conc(int arr[], int si, int mid, int ei) {
+        int marg[] = new int[ei - si + 1];
+        int indx1 = si;
+        int indx2 = mid + 1;
+        int x = 0;// this is for marge arry
+        while (indx1 <= mid && indx2 <= ei) {
+            if (arr[indx1] > arr[indx2]){
+
+                marg[x++] = arr[indx2++];
+            }else
+            {
+
+                marg[x++] = arr[indx1++];
+            }
+
+        }
+        while (indx1 <= mid) {
+            marg[x++] = arr[indx1++];
+        }
+        while (indx2<=ei) {
+            marg[x++]=arr[indx2++];
+            
+        }
+        //copy
+        for(int i=0,j=si;i<marg.length;i++,j++){
+            arr[j]=marg[i];
+        }
+
+    }
+
+    public static void divide(int arr[], int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int mid = si + (ei - si) / 2;
+        divide(arr, si, mid );
+        divide(arr, mid + 1, ei);
+        conc(arr, si, mid, ei);
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 6, 4, 3, 9, 8, 0 };
+        int n = arr.length;
+        int si = 0;
+        divide(arr, 0, n - 1);
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");            
+        }
+    }
+
+}
